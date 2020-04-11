be77a3ae9d14add3e8f5caf947ac857d56c111ce
Ernesto Echeverria Gonzalez <ernestoechegon@gmail.com>
2020-04-11 15:46:57 +0100

    fix(all): se restauran los datos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {WarframesComponent} from './components/warframes/warframes.component'
+import {PrimariesComponent} from './components/primaries/primaries.component'
+import {SecondariesComponent} from './components/secondaries/secondaries.component'
+import {MeleesComponent} from './components/melees/melees.component'
 import {LoginComponent} from './components/login/login.component'
+import {UserHomeComponent} from './components/user-home/user-home.component'
+import {UsersComponent} from './components/users/users.component'
 
 
 const routes: Routes = [
-{	path:'home', component:WarframesComponent	},
-{	path: '', component: WarframesComponent	},
+{	path:'home', component:UserHomeComponent	},
+{	path: '', component: LoginComponent	},
 {	path: 'login', component: LoginComponent },
+{	path: 'primaries', component: PrimariesComponent	},
+{	path: 'secondaries', component: SecondariesComponent	},
+{	path: 'melee', component: MeleesComponent	},
+{	path: 'warframes', component: WarframesComponent	},
+{	path: 'users', component: UsersComponent	},
 {	path:'**', pathMatch: 'full', redirectTo: ''}];
 
 @NgModule({
--- src/app/components/warframes/warframes.component.ts
+++ src/app/components/warframes/warframes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WarframesService } from '../../services/warframes.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-warframes',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WarframesComponent implements OnInit {
 
-  constructor() { }
+  data:any = {warframes:[]}
+
+  constructor(private redir:Router, private warframesSrv:WarframesService) { }
 
   ngOnInit(): void {
+    if(!localStorage.getItem('rhusr')) {
+      this.redir.navigate(['/login'])
+    } else {
+    	this.warframesSrv.getSaved().subscribe(res=> {
+    		this.data = res as any
+    	})
+
+    	this.warframesSrv.getWarframes().subscribe(res=> {
+    		let warframes = res as Array<any>
+    		for(let warframe of warframes) {
+    			if(!this.data.warframes.some(x => x.name == warframe.name)) {
+    				this.data.warframes.push({
+    					name: warframe.name,
+    					imageName: warframe.imageName,
+    					obtained:0
+    				})
+    			}
+    		}
+    		this.data.warframes.sort((a,b) => {
+    			if(a.name < b.name) return -1
+    				return 1
+    		})
+    	})
+    }
+  }
+
+  saveChanges() {
+  	this.warframesSrv.save(this.data).subscribe(res => {})
   }
 
 }
