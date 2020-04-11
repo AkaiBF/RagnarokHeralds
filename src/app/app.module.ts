91cbc2611a7b89d467c51d7e7ca568df97cc673a
Ernesto Echeverria Gonzalez <ernestoechegon@gmail.com>
2020-04-11 15:53:34 +0100

    fix(all): se recupera el app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { WarframesComponent } from './components/warframes/warframes.component';
+import { PrimariesComponent } from './components/primaries/primaries.component'
+import { SecondariesComponent } from './components/secondaries/secondaries.component'
+import { MeleesComponent } from './components/melees/melees.component'
+import { UserHomeComponent } from './components/user-home/user-home.component'
+import { UserNavbarComponent } from './components/user-navbar/user-navbar.component'
+import { UsersComponent } from './components/users/users.component'
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarframesComponent,
+    PrimariesComponent,
+    SecondariesComponent,
+    MeleesComponent,
+    UserHomeComponent,
+    UserNavbarComponent,
+    UsersComponent,
     LoginComponent
   ],
   imports: [
